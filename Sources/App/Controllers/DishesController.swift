28daeb99e88fdff226272c1e1103df7ff1570499
reinual <michel.launier@icloud.com>
2018-06-30 16:27:03 -0400

    final

--- Sources/App/Controllers/DishesController.swift
+++ Sources/App/Controllers/DishesController.swift
@@ -9,10 +9,38 @@ import Vapor
 import FluentSQLite
 
 /// Controls basic CRUD operations on `Dish`s.
-final class DishesController {
+final class DishesController : RouteCollection {
 
+    func boot(router: Router) throws {
+        
+        let dishesRoute = router.grouped("api/dishes")
+        
+        dishesRoute.get("/", use: getAll)
+        dishesRoute.get(Dish.parameter, use: getById)
+        dishesRoute.get("course", String.parameter, use: getByCourse)
+        dishesRoute.delete(Dish.parameter, use: deleteById)
+        dishesRoute.post(Dish.self, at: "/", use: add)
+    }
+    
     func getAll(req : Request) -> Future<[Dish]> {
         return Dish.query(on: req).all()
     }
+    
+    func getById(req : Request) throws -> Future<Dish> {
+        return try req.parameters.next(Dish.self)
+    }
+    
+    func getByCourse(req : Request) throws -> Future<[Dish]> {
+        let course = try req.parameters.next(String.self)
+        return Dish.query(on: req).filter(\.course == course).all()
+    }
+    
+    func deleteById(req : Request) throws -> Future<Dish> {
+        return try req.parameters.next(Dish.self).delete(on: req)
+    }
+    
+    func add(req : Request, dish : Dish) throws -> Future<Dish> {
+        return dish.save(on: req)
+    }
 }
-//
+
